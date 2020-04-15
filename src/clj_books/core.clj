462bea7cc572654bbbfc9c77b6a9024fe653a333
Orlando Mendoza <orlandomr27@gmail.com>
2020-04-14 20:42:34 -0600

    first commit

---
+++ src/clj_books/core.clj
@@ -0,0 +1,38 @@
+(ns clj-books.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.handler.dump :refer [handle-dump]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]])
+  (:gen-class))
+
+(defn greet [req]
+  {:status 200 :body "Olha a cara do bis!" :headers {}})
+
+(defn goodbye [req]
+  {:status 200
+   :body "Goodbye, Cruel World!"
+   :headers {}})
+
+(defn yo [req]
+  (let [name (get-in req [:route-params :name])]
+       {:status 200
+        :body (str "Yo! " name)
+        :headers {}}))
+
+(defn isbn [req]
+  (let [isbn (get-in req [:route-params :isbn])]
+    ))
+
+(defroutes app
+  (GET "/" [] greet)
+  (GET "/goodbye" [] goodbye)
+  (GET "/yo/:name" [] yo)
+  (GET "/request" [] handle-dump)
+  (not-found "Page not found"))
+
+(defn -main [port]
+  (jetty/run-jetty app {:port (Integer. port)}))
+
+(defn -dev-main [port]
+  (jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
---
+++ src/clj_books/draft.clj
@@ -0,0 +1,6 @@
+(ns clj-books.draft)
+
+(deftype Person [name])
+;; => Syntax error compiling at (draft.clj:3:1).
+;;    Unable to resolve symbol: deftype in this context
+
