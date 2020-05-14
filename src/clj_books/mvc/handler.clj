db1145f9a55e02e9d0394a57cc0a4b91d9b4d96d
Orlando Mendoza <orlandomr27@gmail.com>
2020-05-14 09:47:25 -0600

    ratings and book cover added to book page

--- src/clj_books/mvc/handler.clj
+++ src/clj_books/mvc/handler.clj
@@ -1,10 +1,10 @@
 (ns clj-books.mvc.handler
-  (:require [clj-books.mvc.model :as model]
-            [selmer.parser :as parser]
-            [clj-books.config :refer [db-spec]]
-            [clojure.java.jdbc :as jdbc]
+  (:require [clj-books.mvc.services :refer [get-good-reads-review
+                                            get-book-cover]]
+            [clj-books.mvc.model :as model]
+            [clj-books.validators.user-validator :as v])
+  (:require [selmer.parser :as parser]
             [ring.util.response :refer [redirect]]
-            [clj-books.validators.user-validator :as v]
             [clojure.walk :refer [keywordize-keys]]
             [buddy.hashers :as hashers])
   (:gen-class))
@@ -15,36 +15,51 @@
     (parser/render-file "templates/index.html" {:session (:session req) :books books})))
 
 (defn handle-search-index [req]
-  (let [username (get-in req [:session :username])
-        db (:clj-books/db req)
+  (let [db (:clj-books/db req)
         search-by (get-in req [:form-params "search-by"])
         search-text (get-in req [:form-params "search-text"])
         books (model/select-books db search-by search-text)]
-    (parser/render-file "templates/index.html" {:session (:session req) :books books})))
+    (parser/render-file "templates/index.html" {:session (:session req)
+                                                :books books})))
 
-(defn handle-isbn-details [req]
-  ;; TODO
-  {:status 200
-   :headers {}
-   :body ""})
+(defn handle-book-page [req]
+  (let [db (:clj-books/db req)
+        search-by "isbn"
+        search-text (get-in req [:params :isbn])
+        isbn (get-in req [:params :isbn])
+        book (first (model/select-books db search-by search-text))
+        book-cover (get-book-cover isbn)
+        book-ratings (get-good-reads-review isbn)
+        ratings-count (get book-ratings :work_ratings_count)
+        avg-rating (get book-ratings :average_rating)]
+    (clojure.pprint/pprint req)
+    (clojure.pprint/pprint book)
+    (clojure.pprint/pprint book-cover)
+    (clojure.pprint/pprint ratings-count)
+    (parser/render-file "templates/isbn.html" {:session (:session req)
+                                               :book book
+                                               :ratings-count ratings-count
+                                               :avg-rating avg-rating
+                                               :cover book-cover
+                                               :selmer/context "/"})))
 
 (defn handle-create-review [req]
   ;; TODO
-  {:status 200
+  {:status  200
    :headers {}
-   :body ""})
+   :body    ""})
 
 (defn handle-api-isbn [req]
-  {:status 200
+  {:status  200
    :headers {}
-   :body ""})
+   :body    ""})
 
 (defn login [req]
   (parser/render-file "templates/login.html" {:title "Login"}))
 
-(defn login-submit [{form :form-params
+(defn login-submit [{form    :form-params
                      session :session
-                     :as req}]
+                     :as     req}]
   (let [{:strs [email password]} form
         db (:clj-books/db req)]
     (if-let [user (model/login-user db email)]
@@ -70,7 +85,7 @@
 (defn register [req]
   (let [current-user (get-in req [:session :current-user])]
     (parser/render-file "templates/register.html"
-                        {:title "Register"
+                        {:title        "Register"
                          :current-user current-user})))
 
 (defn register-submit [{form :form-params :as req}]
@@ -81,15 +96,15 @@
       (do
         (if (model/control-user-by-email db email)
           (parser/render-file
-           "templates/register.html"
-           {:messages {"danger" "Email already registered, please provide another email!"}})
-           ;; inserts the new user in the database
+            "templates/register.html"
+            {:messages {"danger" "Email already registered, please provide another email!"}})
+          ;; inserts the new user in the database
           (let [user-id (model/register-user! db first-name last-name email password)
                 books (model/select-books db)]
             (parser/render-file
-             "templates/index.html"
-             {:title ""
-              :messages {"success" "Your are successfully registered!"
-                         "warning" "Please login to write a review"}
-                                                        :books books}))))
+              "templates/index.html"
+              {:title    ""
+               :messages {"success" "Your are successfully registered!"
+                          "warning" "Please login to write a review"}
+               :books    books}))))
       (parser/render-file "templates/register.html" (assoc form :errors errors)))))
